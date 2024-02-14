ea9c3b25271021d73084a3502b9196a02e88727a
Touqeer Ansari <126690073+Touqeer001@users.noreply.github.com>
2024-02-15 00:10:26 +0530

    Add files via upload

---
+++ utils/upload.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+// import pkg from "multer-gridfs-storage";
+
+// const { GridFsStorage } = pkg;
+import  GridFsStorage  from "multer-gridfs-storage";
+//import { GridFsStorage } from "multer-gridfs-storage";
+
+import dotenv from "dotenv";
+dotenv.config();
+const username = process.env.DB.USERNAME;
+const password = process.env.DB.PASSWORD;
+
+const storage = new GridFsStorage({
+  
+  url:  `mongodb+srv://touqeeransari:Ansari@cluster0.ucoo2jo.mongodb.net/?retryWrites=true&w=majority`,
+  options: { useNewUrlParser: true },
+  file: (request, file) => {
+    const match = ["image/png", "image/jpg"];
+
+    if (match.indexOf(file.memeType) === -1)
+      return `${Date.now()}-blog-${file.originalname}`;
+
+    return {
+      bucketName: "photos",
+      filename: `${Date.now()}-blog-${file.originalname}`,
+    };
+  },
+});
+
+export default multer({ storage });
